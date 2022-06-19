2526de9938cb69be535750b6465074460bec1665
Denis <96013977+xdeanboy@users.noreply.github.com>
2022-06-20 00:45:56 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,6 @@ return [
     '~^film/(\d+)$~' => [\Test\Controllers\FilmsController::class, 'viewById'],
     '~^film/(\d+)/delete$~' => [\Test\Controllers\FilmsController::class, 'delete'],
     '~^film/add$~' => [\Test\Controllers\FilmsController::class, 'add'],
-    '~^test$~' => [\Test\Controllers\FilmsController::class, 'test'],
     '~^sign-in$~' => [\Test\Controllers\UsersController::class, 'signIn'],
     '~^logout$~' => [\Test\Controllers\UsersController::class, 'logOut'],
 ];
